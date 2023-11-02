2c303b612f905829f16c24c04d7c3835896dff0f
fikriislamyy <fikriislamyy@gmail.com>
2023-11-02 12:18:35 +0700

    feat(controllers): add user registration

---
+++ controllers/userController.go
@@ -0,0 +1,55 @@
+package controllers
+
+import (
+	"learn-conventional-commits/initializers"
+	"learn-conventional-commits/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Register(c *gin.Context) {
+	var body struct {
+		Firstname string
+		Lastname string
+		Email string
+		Password string
+		Phone string
+	}
+
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body",
+		})
+		return
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to hash password",
+		})
+		return
+	}
+
+	user := models.User{
+		Firstname: body.Firstname,
+		Lastname: body.Lastname,
+		Email: body.Email,
+		Password: string(hash),
+		Phone: body.Phone,
+	}
+
+	result := initializers.DB.Create(&user)
+	if result.Error != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to create user",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "User created successfully",
+	})
+}
