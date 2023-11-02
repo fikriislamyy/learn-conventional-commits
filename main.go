c62230eefd3cf9f87cea709a5262770042728095
fikriislamyy <fikriislamyy@gmail.com>
2023-11-02 15:02:52 +0700

    feat(api): add login, logout, and user endpoint

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"learn-conventional-commits/controllers"
 	"learn-conventional-commits/initializers"
+	"learn-conventional-commits/middleware"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,10 +12,14 @@ func init() {
 	initializers.LoadEnv()
 	initializers.ConnectToDB()
 	initializers.SyncDb()
+	initializers.InitializeRedisClient()
 }
 
 func main() {
 	r := gin.Default()
 	r.POST("/register", controllers.Register)
+	r.POST("/login", controllers.Login)
+	r.GET("/user", middleware.SessionMiddleware() ,controllers.GetUserData)
+	r.POST("/logout", middleware.SessionMiddleware(),controllers.Logout)
 	r.Run()
 }
